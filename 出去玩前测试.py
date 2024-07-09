eaafec0da130aa5649e62be7dcb02cbbc2dce3f0
TinkyWinky0227 <FrankSong227@outlook.com>
2024-07-09 14:06:18 +0800

    Add files via upload

---
+++ 出去玩前测试.py
@@ -0,0 +1,5 @@
+mood_index =float(input('母亲大人今天的心情指数为：'))
+if mood_index >=60:
+    print('恭喜，今天可以爽玩一天。')
+else:
+    print('明哲保身，保命要紧，还是别去玩了。')
---
+++ 高斯函数求值.py
@@ -0,0 +1,4 @@
+print('高斯函数：y=[x]')
+num=float(input('请输入x的值为：'))
+a=int(num)
+print('对应的y的值为：',str(a))
